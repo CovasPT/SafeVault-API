34b0bad81e58b4091e0407c22cde0c3e738ede31
Tiago Covas <tiagocovas2004@gmail.com>
2026-02-09 02:24:55 +0000

    FIRST

---
+++ src/main/java/com/tiagocovas/safevaultapi/SafevaultApiApplication.java
@@ -0,0 +1,13 @@
+package com.tiagocovas.safevaultapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafevaultApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafevaultApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagocovas/safevaultapi/SafevaultApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagocovas.safevaultapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafevaultApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
